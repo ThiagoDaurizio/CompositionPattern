829ccd9a81344b6fed4f55f1925a37f1aa685d16
Thiago Daurizio <Thiago_daurizio@hotmail.com>
2023-07-20 12:50:23 -0300

    feature: modals different examples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,113 @@
 'use client'
 
 import { Modal } from "@/compositions/Modal"
+import ModalContent from "@/compositions/Modal/ModalContent"
 import { useState } from "react"
 
 export default function Home() {
   const [isOpened, set_isOpened] = useState<boolean>(false)
 
+
+  const [isOpenedClassic, set_isOpenedClassic] = useState<boolean>(false)
+  const [isOpenedAdvise, set_isOpenedAdvise] = useState<boolean>(false)
+  const [isOpenedDecision, set_isOpenedDecision] = useState<boolean>(false)
+
+
   const handleConfirm = () => {
     window.alert('confirmed')
-    set_isOpened(false)
+    set_isOpenedClassic(false)
+    set_isOpenedDecision(false)
   }
 
   return (
     <main className="bg-slate-100 min-h-screen flex justify-center pt-8">
-      <button 
-        onClick={() => set_isOpened(true)}
-        className="bg-slate-600 rounded-md text-slate-100 h-[32px] w-[100px]"
-      >Open Modal</button>
+      <ul className="flex gap-2">
+        <li>
+          <button 
+            onClick={() => set_isOpenedClassic(true)}
+            className="bg-slate-600 rounded-md text-slate-100 h-[32px] w-[175px]"
+          >
+            Open Modal Classic
+          </button>
+        </li>
+
+        <li>
+          <button 
+            onClick={() => set_isOpenedAdvise(true)}
+            className="bg-slate-600 rounded-md text-slate-100 h-[32px] w-[175px]"
+          >
+            Open Modal Advise
+          </button>
+        </li>
+
+        <li>
+          <button 
+            onClick={() => set_isOpenedDecision(true)}
+            className="bg-slate-600 rounded-md text-slate-100 h-[32px] w-[175px]"
+          >
+            Open Modal Decline
+          </button>
+        </li>
+      </ul>
+
+    {isOpenedClassic && 
+      (<Modal.Root>
+        <Modal.Header>
+          <Modal.Title>
+            Modal Classic Title
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Content>
+          Modal content here
+        </Modal.Content>
+
+        <Modal.Footer>
+          <Modal.Action color="success" onClick={handleConfirm}>
+            OK
+          </Modal.Action>
+          <Modal.Action color='error' onClick={() => set_isOpenedClassic(false)}>
+            Cancel
+          </Modal.Action>
+        </Modal.Footer>
+      </Modal.Root>
+    )}
+
+    {isOpenedAdvise && 
+      (<Modal.Root>
+        <ModalContent>
+          Modal advise here
+        </ModalContent>
+
+        <Modal.Footer>
+          <Modal.Action color='info' onClick={() => set_isOpenedAdvise(false)}>
+            Ok
+          </Modal.Action>
+        </Modal.Footer>
+      </Modal.Root>
+    )}
+
+    {isOpenedDecision && 
+      (<Modal.Root>
+        <ModalContent>
+          You is alright of this?
+        </ModalContent>
+
+        <Modal.Footer>
+          <Modal.Action color='success' onClick={handleConfirm}>
+            YES
+          </Modal.Action>
+          <Modal.Action color='error' onClick={() => set_isOpenedDecision(false)}>
+            NO
+          </Modal.Action>
+        </Modal.Footer>
+      </Modal.Root>
+      )}
+
+
 
       {isOpened && 
         (<Modal.Root backdropClose onClose={() => set_isOpened(false)}>
-          <Modal.Header>
-            <Modal.Title>
-              Modal Title
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Content>
-            Modal Content
-
-          </Modal.Content>
-          <Modal.Footer>
-            <Modal.Action 
-              color={'success'}
-              onClick={handleConfirm}
-            >OK</Modal.Action>
-            <Modal.Action 
-              color={'error'}
-              onClick={() => set_isOpened(false)}
-            >Cancel</Modal.Action>
-          </Modal.Footer>
         </Modal.Root>
       )}
     </main>
